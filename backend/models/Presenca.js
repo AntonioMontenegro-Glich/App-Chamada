e1e33f409ab3e0fb909634483f0f45186440bc38
AntonioMontenegro-Glich <antoniolemos2004@gmail.com>
2024-09-14 20:27:19 -0300

    Comentando para estudos no Squema

--- backend/models/Presenca.js
+++ backend/models/Presenca.js
@@ -1,5 +1,7 @@
+//Importando o Banco de Dados para Fazer os Esquemas 
 const mongoose = require('mongoose');
-
+//Os atributos utilizados do sistema estarão atribuidos dentro de chaves
+//Tudo que irei exibir na tela do meu sistema estará configurado com o tipo(type) e se ela é obrigatório(required: true or false)
 const PresencaSchema = new mongoose.Schema({
     nome: { type: String, required: true },
     resumo_aula: { type: String, required: true },
@@ -11,5 +13,5 @@ const PresencaSchema = new mongoose.Schema({
     photo: { type: String, required: true },
     created_at: { type: Date, default: Date.now }
 });
-
+//Exportando o Esquema para utilizar nas outras partes do Meu Sistema como no meu Controlador que irá fazer operações com o esquema
 module.exports = mongoose.model('Presenca', PresencaSchema);
